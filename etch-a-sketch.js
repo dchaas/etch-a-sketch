8ff564ad70f9163cd901e5af97b207692a4e93bc
Daniel Haas <daniel.c.haas@gmail.com>
2022-05-28 07:08:25 -0400

    Dynamic grid generation. Added dynamic grid generation to accomodate a user selection of any size

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,16 +1,38 @@
-// create a 16x16 grid of squares
-let grid = []
+const resetBtn = document.querySelector("#reset");
+const PIXELS = 800;
 const container = document.querySelector('.container');
 
-for (let i=0; i<16; i++) {
-    const block = document.createElement('div');
-    block.textContent = `${i}`
-    block.classList.add('grid');
-    block.addEventListener('mouseover', hoverStyle);
+resetBtn.addEventListener('click',() => {
+    let gridSize = parseInt(prompt('How big do you want the grid? Enter rows: (max 100) ',''));
+    if (gridSize>100) {
+        gridSize=100;
+        alert('Grid too large, defaulting to max size of 100');
+    } 
+    drawBoard(gridSize);
+});
 
-    container.appendChild(block);
-    grid.push(block);
+function drawBoard(size) {
+    // create a 16x16 grid of squares
+    let grid = []
+    // remove all children node before repeating
+    removeChildren(container);
+
+
+    for (let i=0; i<size*size; i++) {
+        const block = document.createElement('div');
+        block.textContent = `${i}`
+        block.classList.add('grid');
+        block.addEventListener('mouseover', hoverStyle);
+
+        // resize the container to maintain 960x960px
+        container.style.gridTemplateColumns = `repeat(${size},${PIXELS/size}px)`;
+        container.style.gridTemplateRows = `repeat(${size},${PIXELS/size}px)`;
+        container.appendChild(block);
+        grid.push(block);
+    }
 }
+
+drawBoard(4);
 
 function hoverStyle(event) {
     
@@ -20,4 +42,8 @@ function hoverStyle(event) {
     event.target.classList.toggle('drawn');
 }
 
-//grid[0].addEventListener('mouseover', hoverStyle);
+function removeChildren(node) {
+    while (node.firstChild) {
+        node.removeChild(node.firstChild);
+    }
+}
