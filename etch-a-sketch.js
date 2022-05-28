6fe7bde31d14ecc4c02f2f0ddcc79f283adcf85e
Daniel Haas <daniel.c.haas@gmail.com>
2022-05-28 06:46:13 -0400

    Mouseover interaction added. Mouseover event listener added to color the grid element when it's moused over. Works on any element in the grid.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,12 +1,23 @@
 // create a 16x16 grid of squares
 let grid = []
 const container = document.querySelector('.container');
+
 for (let i=0; i<16; i++) {
     const block = document.createElement('div');
     block.textContent = `${i}`
     block.classList.add('grid');
+    block.addEventListener('mouseover', hoverStyle);
+
     container.appendChild(block);
     grid.push(block);
 }
 
-//function hoverStylin
+function hoverStyle(event) {
+    
+    console.log('Moused over');
+    console.log(event)
+    console.log(event.target)
+    event.target.classList.toggle('drawn');
+}
+
+//grid[0].addEventListener('mouseover', hoverStyle);
